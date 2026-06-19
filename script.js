095cf73a8bf1859e23786cad0ad114aeba2daf96
Vinicius Baptista <viniciusb_s@hotmail.com>
2026-06-19 20:50:26 -0300

    Correções

--- script.js
+++ script.js
@@ -304,4 +304,8 @@ const app = {
     }
 };
 
-window.onload = () => app.init();
+if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', () => app.init());
+} else {
+    app.init();
+}
